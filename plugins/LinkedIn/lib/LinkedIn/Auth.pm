0a00cd3bde152b812196179b19af22f788d7c6db
Shmuel Fomberg <shmuelfomberg@gmail.com>
2011-10-28 10:56:22 +0900

    continued implementing

--- plugins/LinkedIn/lib/LinkedIn/Auth.pm
+++ plugins/LinkedIn/lib/LinkedIn/Auth.pm
@@ -23,39 +23,87 @@ sub condition {
     $$reason = "Please install these Perl modules: ".join(", ", @not_found);
     return 0;
 }
+use Data::Dumper;
 
 sub login {
+    my $class    = shift;
+    my ($app)    = @_;
+    my $q        = $app->param;
+
     require Net::OAuth;
     require HTTP::Request::Common;
-    my $ua = MT->new_ua( { paranoid => 1 } );
+    my $ua = $app->new_ua( { paranoid => 1 } );
 
     my $request = Net::OAuth->request("request token")->new(
         consumer_key => 'h7rnj3ecjbs9',
         consumer_secret => 'N1Pie5VHq8yeZQjL',
-        request_url => 'https://www.linkedin.com/uas/oauth/authenticate',
+        request_url => 'https://api.linkedin.com/uas/oauth/requestToken',
         request_method => 'POST',
         signature_method => 'HMAC-SHA1',
         timestamp => time(),
         nonce => 'hsu94j3884jdopsl',
-        callback => 'http://printer.example.com/request_token_ready',
+        callback => __create_return_url($app),
         protocol_version => $Net::OAuth::PROTOCOL_VERSION_1_0A,
-        extra_params => {
-            __mode => 'bar'
-        },
     );
 
     $request->sign;
 
-    my $res = $ua->request(POST $request->to_url); # Post message to the Service Provider
+    my $res = $ua->request(HTTP::Request::Common::POST($request->to_url)); # Post message to the Service Provider
 
     if ($res->is_success) {
+        print STDERR "Request is success\n";
+        print STDERR Dumper($res);
         my $response = Net::OAuth->response('request token')->from_post_body($res->content);
-        print "Got Request Token ", $response->token, "\n";
-        print "Got Request Token Secret ", $response->token_secret, "\n";
+        print STDERR "Got Request Token ", $response->token, "\n";
+        print STDERR "Got Request Token Secret ", $response->token_secret, "\n";
     }
     else {
-        die "Something went wrong";
+        print STDERR "Something went wrong\n";
+        print STDERR Dumper($res);
     }
+    # redurect to https://api.linkedin.com/uas/oauth/authorize? oauth_token=f7868c3a-7336-4662-a6d1-3219fb4650d1
+}
+
+sub commenter_auth_params {
+    my ($key, $blog_id, $entry_id, $static) = @_;
+    require MT::Util;
+    if ($static =~ m/^http%3A%2F%2F/) {
+        # the URL was encoded before, but we want the normal version
+        $static = MT::Util::decode_url($static);
+    }
+    my $params = {
+        blog_id => $blog_id,
+        static  => $static,
+    };
+    $params->{entry_id} = $entry_id if defined $entry_id;
+    return $params;
+}
+
+sub __create_return_url {
+    my $app = shift;
+    my $q        = $app->param;
+    my $cfg = $app->config;
+
+    my $cgi_path = $app->config('CGIPath');
+    $cgi_path .= '/' unless $cgi_path =~ m!/$!;
+
+    my $blog_id = $q->param("blog_id");
+    $blog_id =~ s/\D//g;
+    my $static = $q->param("static");
+    
+    my @params = (
+        "__mode=handle_sign_in",
+        "key=LinkedIn",
+        "blog_id=$blog_id",
+        "static=".MT::Util::encode_url($q->param("static")),
+    );
+
+    if (my $entry_id = $q->param("entry_id")) {
+        $entry_id =~ s/\D//g;
+        push @params, "entry_id=$entry_id";
+    }
+    
+    return $cgi_path . $cfg->CommentScript ."?". join('&', @params);
 }
 
 1;
